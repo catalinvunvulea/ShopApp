f22776e1c0203603bf2b2572bd6660001aec9554
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-01 15:13:33 +0100

    ad more elements ot ProductDetailScreen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -16,11 +16,46 @@ class ProductDetailScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as String; //is the id!
     final loadedProduct = Provider.of<Products>(
       context,
-      listen: false, //this will stop update of this widget every time when there is a change in the Products; it is not requiered in thuis case
+      listen:
+          false, //this will stop update of this widget every time when there is a change in the Products; it is not requiered in thuis case
     ).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '£${loadedProduct.price}',
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                '${loadedProduct.description}',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
