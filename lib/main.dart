c93127bbcd6c3fe45600ec1d872f1a68aed49201
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-01 10:02:50 +0100

    add MultiProvider
    Enable us to add more than one ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart'; //enable us to set a provider
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './provider/products_provider.dart';
+import './provider/cart_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,8 +14,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider( 
-      create: (ctx) => Products(), //now we have an instance of Products class which applyes to all it's child and his children (MaterialApp is the child - the route of the app, but extends to it's children, who can now also have listeners)
+    return MultiProvider( //enable us to have multiple ChangeNotifierProvider (listeners for different providers anywhere in the app (anywhere as we have tis in Main))
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) =>
+              Products(), //now we have an instance of Products class which applyes to all it's child and his children (MaterialApp is the child - the route of the app, but extends to it's children, who can now also have listeners)
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        )
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
