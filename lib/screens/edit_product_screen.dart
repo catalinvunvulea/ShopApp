dae946122cafeaf9cb268ce921d7bda6b4c9305b
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-02 17:18:36 +0100

    manage Form Inputs Focus
    Ex: jumpt to other TextFormField when ok btn from keybord pressed

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
-
   static const rotueName = '/edit-product-screen';
-  
+
   @override
   _EditProductScreenState createState() => _EditProductScreenState();
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
-
-
+  final _priceFocusNode =
+      FocusNode(); //object that can be used by statefull widgets to obtain keyboard focus and keybod events
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +23,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
               children: <Widget>[
                 TextFormField(
                   decoration: InputDecoration(labelText: 'Title'),
-                  textInputAction: TextInputAction
-                      .next, //controlls the action of button from the keyoard as an ok, next, done. submit etc
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) {
+                    //action of the ok/done button from the keyboard
+                    FocusScope.of(context).requestFocus(
+                        _priceFocusNode); //we tell the cursore to jump to a different TextFormField when action btn from keybord press (in this case it wil jump to _priceFocusNode)
+                  },
+                ), //controlls the action  button (only the display, not the aciton yet)from the keyoard as an ok, next, done. submit etc
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Price'),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  focusNode: _priceFocusNode, // kind of a identifier**
                 ),
               ],
             ),
