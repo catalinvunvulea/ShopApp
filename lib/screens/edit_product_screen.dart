663c41c67867da9da4727d3b5c902631f743a62a
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-03 09:19:37 +0100

    Image Url input from TextFormField
    add listener to a TextFormField and user a custom function when the focus (user tapped somwhere else) is moved

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -11,23 +11,46 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode =
       FocusNode(); //object that can be used by statefull widgets to obtain keyboard focus and keybod events (like an identifier) / see where is called
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlController =
+      TextEditingController(); //we need acces to the user input Url to see the picture (before the form is submited; once the form is submitted, we would have acces without this controller)
+  final _imageUrlFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocusNode.addListener(_updateImageUrl); //we will run the _updateImageUrl func when the focus is moved from _imageUrlFocusNode
+
+  }
 
   @override
   void dispose() {
     super.dispose();
-    _priceFocusNode.dispose(); //once we leave the screen we always have to despose FocusNodes otherwise they will stick in the memory 
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _priceFocusNode
+        .dispose(); //once we leave the screen we always have to despose FocusNodes otherwise they will stick in the memory
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) { //if the focus (user tapped somewhere else) was moved from this textFormField, we rebuild the screen so we can see the image
+    setState(() {
+    });
+    }
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Edit title'),
+          title: Text('Edit product'),
         ),
         body: Padding(
           padding: EdgeInsets.all(16),
           child: Form(
+            //gives you acces to the values of the user Input (of TextFormField) without having to add your own textEditinControllers
             child: ListView(
               children: <Widget>[
                 TextFormField(
@@ -57,6 +80,42 @@ class _EditProductScreenState extends State<EditProductScreen> {
                       3, //no of lines shown on the screen (you can write more, but you will have to scroll to see them)
                   focusNode: _descriptionFocusNode,
                 ),
+                Row(
+                  // has an unconstrainer width
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(top: 8, right: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      child: _imageUrlController.text.isEmpty
+                          ? Center(child: Text('Enter a URL'))
+                          : FittedBox(
+                              child: Image.network(
+                                _imageUrlController.text,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        //takes all the available width, and as it is in a row, we need to wrap in Expanded
+                        decoration: InputDecoration(hintText: 'Image Url'),
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        controller:
+                            _imageUrlController, //we need to use the value before the form is subbmited, hence we add a controller (to use the value Inputed by user)
+                        focusNode: _imageUrlFocusNode, //in this case we add the focusNode to know when the user unselected this text field, for that we need a listener, added in the init state
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
