7983bdc1dba9e68819d5c13702f297cc45948e75
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-04 18:42:24 +0100

    PULL TO REFRESH
    RefreshIndicator (onRefresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -7,8 +7,12 @@ import '../provider/products_provider.dart';
 import '../widgets/user_product_item.dart';
 
 class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products-screen';
 
-static const routeName = '/user-products-screen';
+  Future<void> _refreshProducts(BuildContext context) async {
+    //as this is not in a statefull widget, and not in build, we need to pass argument BuildCOntext to use it below;
+    await Provider.of<Products>(context).fetchAndSetProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +31,13 @@ static const routeName = '/user-products-screen';
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-                  child: ListView.builder(
+      body: RefreshIndicator(
+        //returns a future, shows a spinner indicator by default;
+        onRefresh: () => _refreshProducts(
+            context), //we need to tell it when to stop showing the spinner; returns a future and we build the method accordingly
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView.builder(
             itemCount: productsData.items.length,
             itemBuilder: (ctx, index) => UserProducItem(
               productsData.items[index].title,
@@ -38,6 +46,7 @@ static const routeName = '/user-products-screen';
             ),
           ),
         ),
+      ),
     );
   }
 }
