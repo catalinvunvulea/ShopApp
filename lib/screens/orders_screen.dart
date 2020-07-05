54335506faaf5a8afe02d6d0ece3870b7a0e6bc5
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-05 14:21:51 +0100

    Improve code using FutureBuilder
    no longer requiered to change to StatefullWidget and manage isLoading in initState or didChangeDependencies

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,51 +5,45 @@ import '../widgets/app_drawer.dart';
 import '../provider/orders_provider.dart';
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget  {
   const OrdersScreen({Key key}) : super(key: key);
 
   static const routeName = '/orders-screen';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      //this line of code is used (if we dont use didChangeDependencies - we should use this) as setAndFetchOrders returns a future, and initState runs only at the beginning and using this line of code it will wait for what is comming after then, and re-run
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).setAndFetchOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
+ 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('My orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder( //read description
+        future: Provider.of<Orders>(context, listen: false).setAndFetchOrders(), //data to expect
+        builder: (ctx, dataReturnedByFuture) { //builder: context and the data received from future
+          if (dataReturnedByFuture.connectionState == ConnectionState.waiting) { //connection state - can check if it is done, still waiting, etc and depending on the result, we can return widget on screen
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) => OrderItem(
-                orderData.orders[index],
-              ),
-              itemCount: orderData.orders.length,
-            ),
+            );
+          } else {
+            if (dataReturnedByFuture.error != null) {
+              return Center(
+                child: Text('An error has occured'),
+              );
+            } else {
+              if (dataReturnedByFuture.connectionState ==
+                  ConnectionState.done) {
+                return Consumer<Orders>(
+                  builder: (ctx, orderData, child) => ListView.builder(
+                      itemBuilder: (ctx, index) => OrderItem(
+                            orderData.orders[index],
+                          ),
+                      itemCount: orderData.orders.length),
+                );
+              }
+            }
+          }
+        },
+      ),
     );
   }
 }
