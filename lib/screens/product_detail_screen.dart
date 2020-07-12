25dd93b4f0d3aa481b9fdf5bc12620f503b07742
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-07-12 09:42:56 +0100

    working with SLIVERS - SliverAppBar, SliverList

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -20,45 +20,57 @@ class ProductDetailScreen extends StatelessWidget {
           false, //this will stop update of this widget every time when there is a change in the Products; it is not requiered in thuis case
     ).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero( // used on the previous screen, and needs to be used here as well; use the same tag
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title),
+      // ),
+      body: CustomScrollView(
+        //animate the topest widget into a bar, when scrolling down
+        slivers: <Widget>[
+          //scollable areas on the screen
+          SliverAppBar(
+            expandedHeight:
+                300, //the height that should have if it'snot the app bar but a widget (immage in our case )
+            pinned:
+                true, //app bar will always be visible and will not scroll out off the view
+            flexibleSpace: FlexibleSpaceBar(
+              title: Container(decoration: BoxDecoration(color: Colors.purple) ,child: Text(loadedProduct.title)),
+              background: Hero(
+                // used on the previous screen, and needs to be used here as well; use the same tag
                 tag: loadedProduct.id,
                 child: Image.network(
                   loadedProduct.imageUrl,
                   fit: BoxFit.contain,
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              '£${loadedProduct.price}',
-              style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                '${loadedProduct.description}',
+            ), //what is inside the app barr and how it may change
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(height: 10),
+              Text(
+                '£${loadedProduct.price}',
                 style: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 20,
-                ),
-                textAlign: TextAlign.center,
+                    color: Colors.grey,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
               ),
-            )
-          ],
-        ),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  '${loadedProduct.description}',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 800) // added only to make a longer screen to be able to see customScrollView (if description of the product would be longer)
+            ]),
+          ),
+        ],
       ),
     );
   }
